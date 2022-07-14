5d9e035f1f0d9d29ceb29f7c3adcd149115991a7
ssembatya-dennis <ssembatyadennis25@gmail.com>
2022-07-14 07:06:26 +0300

    feat(navigation-bar): change the navigation-bar background on scroll change the background-color on scroll which marks the first use case of state in the App.

--- netflix-clone/src/components/SubNavDrop.js
+++ netflix-clone/src/components/SubNavDrop.js
@@ -5,7 +5,7 @@ class SubNavDropdown extends React.Component {
       return (
         <div className='subnav-element'>
           <span className='notification'>
-          <img class="profile-icon" src="https://occ-0-6517-32.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdcxMzRQIm7B1VbSEQ6Gzbh547sBcWOewiM6BYM1RpDbqtkb-Wt_r9tEQuiI216ZlFv7tGdvn5nnY2CCYIM-TtPu_uhOVK4.png?r=e6c" alt="" />
+          <img className="profile-icon" src="https://occ-0-6517-32.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdcxMzRQIm7B1VbSEQ6Gzbh547sBcWOewiM6BYM1RpDbqtkb-Wt_r9tEQuiI216ZlFv7tGdvn5nnY2CCYIM-TtPu_uhOVK4.png?r=e6c" alt="" />
           </span>
         </div>
       )
--- netflix-clone/src/index.js
+++ netflix-clone/src/index.js
@@ -8,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <main className='home-page'>
     <App />
+    </main>
   </React.StrictMode>
 );
 
